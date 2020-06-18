d80c1e5157a9138a95c94a11a13a05276af5ca28
shibli21 <syedshiblimahmud@gmail.com>
2020-06-18 22:48:35 +0600

    remove commented codes

--- app.js
+++ app.js
@@ -71,34 +71,6 @@ app.post("/register", (req, res) => {
     .then((user) => {
       res.json(user[0]);
     });
-
-  //   bcrypt.hash(password, saltRounds, function (err, hash) {
-  //     knex
-  //       .transaction((trx) => {
-  //         trx
-  //           .insert({
-  //             hash: hash,
-  //             email: email,
-  //           })
-  //           .into("login")
-  //           .returning("email")
-  //           .then((loginEmail) => {
-  //             return trx("users")
-  //               .returning("*")
-  //               .insert({
-  //                 email: loginEmail[0],
-  //                 name: name,
-  //                 joined: new Date(),
-  //               })
-  //               .then((user) => {
-  //                 res.json(user[0]);
-  //               });
-  //           })
-  //           .then(trx.commit)
-  //           .catch(trx.rollback);
-  //       })
-  //       .catch((err) => res.status(400).json(err));
-  //   });
 });
 
 app.get("/profile/:id", (req, res) => {
